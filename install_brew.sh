3a3159220fe8f462f562461e337eace14d0c7c6e
Justin Stribling <justinastribling@gmail.com>
2017-04-13 00:00:32 -0400

    updated brew installer and changed some sublime settings

--- install_brew.sh
+++ install_brew.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+which -s brew
+if [[ $? != 0 ]] ; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
 brew install gcc
 brew install git
 brew install htop
