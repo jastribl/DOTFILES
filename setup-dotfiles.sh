2c5f0c71a32b1258158c3d6018abeb48815ccf5a
Justin Stribling <justinastribling@gmail.com>
2017-04-13 03:08:02 -0400

    renamed dotfiles

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,18 +1,17 @@
 # !/bin/bash
 
-files=".bashrc .vimrc .zshrc .gitconfig"
+files="bashrc vimrc zshrc gitconfig"
 
-currentDir=`pwd`
-dotfileDir="$currentDir/dotfiles"
-backupDir="$currentDir/dotfiles_old/backup_$(date +%Y-%m-%d:%H:%M:%S)"
+dotfileDir="$PWD/dotfiles"
+backupDir="$PWD/dotfiles_old/backup_$(date +%Y-%m-%d:%H:%M:%S)"
 
 mkdir -p $backupDir
 
 cd $dotfileDir
 
 for file in $files; do
-    echo "> mv ~/$file $backupDir/"
-    mv ~/$file $backupDir/
-    echo "> ln -s $dotfileDir/$file ~/$file"
-    ln -s $dotfileDir/$file ~/$file
+    echo "> mv ~/.$file $backupDir/"
+    mv ~/.$file $backupDir/
+    echo "> ln -s $dotfileDir/$file ~/.$file"
+    ln -s $dotfileDir/$file ~/.$file
 done
