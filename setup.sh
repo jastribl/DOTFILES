c038085fabe587da7a3c9637501afb3d07b4bacf
Justin Stribling <justinastribling@gmail.com>
2018-04-12 22:50:13 -0700

    cleaned up setup script with choices

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
 # !/bin/bash
 
-./setup-submodules.sh
-./setup-brew.sh
-./setup-zsh.sh
-./setup-vim.sh
-./setup-tmux.sh
-./setup-dotfiles.sh
-./setup-powerline-fonts.sh
-./setup-mac.sh
-./setup-sublime.sh
+installScripts=(
+    setup-submodules.sh
+    setup-brew.sh
+    setup-zsh.sh
+    setup-vim.sh
+    setup-tmux.sh
+    setup-dotfiles.sh
+    setup-powerline-fonts.sh
+    setup-mac.sh
+    setup-sublime.sh
+)
+
+for installScript in "${installScripts[@]}"; do
+    read -p "Would you like to run $installScript (y/n/q)? " choice
+    case "$choice" in
+      y|Y ) echo "running $installScript" && ./$installScript && echo "done" ;;
+      n|N ) echo "moving on" ;;
+      q|Q ) exit ;;
+      * ) echo "invalid option" ;;
+    esac
+done
