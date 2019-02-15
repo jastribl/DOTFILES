99890d73e65e2a5b02f054b614edf8150c98db0e
Justin Stribling <justinastribling@gmail.com>
2019-02-15 16:15:10 -0500

    Added casks to brew analysis

--- brew-dep-analysis.py
+++ brew-dep-analysis.py
@@ -21,16 +21,27 @@ for brew, deps in all_deps.items():
     all_brews = all_brews + [brew] + deps
 
 actual_brews = subprocess.getoutput("brew list").split()
-unused = [brew for brew in actual_brews if brew not in all_brews]
+extra_brews = [brew for brew in actual_brews if brew not in all_brews]
+missing_brews = [brew for brew in expected_brews if brew not in all_brews]
 
-missing = [brew for brew in expected_brews if brew not in all_brews]
 
-if len(unused) > 0 or len(missing) > 0:
-    if len(unused) > 0:
-        print("Extra brews:", ' '.join(unused))
+expected_casks = [line.rstrip('\n') for line in open('./brew/cask-list')]
+actual_casks = subprocess.getoutput("brew cask list").split()
+missing_casks = [cask for cask in expected_casks if cask not in actual_casks]
+extra_casks = [cask for cask in actual_casks if cask not in expected_casks]
 
-    if len(missing) > 0:
-        print("Missing brews:", ' '.join(missing))
+if len(extra_brews) + len(missing_brews) + len(extra_casks) + len(missing_casks) > 0:
+    if len(extra_brews) > 0:
+        print("Extra brews:", ' '.join(extra_brews))
+
+    if len(missing_brews) > 0:
+        print("Missing brews:", ' '.join(missing_brews))
+
+    if len(extra_casks) > 0:
+        print("Extra casks:", ' '.join(extra_casks))
+
+    if len(missing_casks) > 0:
+        print("Missing casks:", ' '.join(missing_casks))
 
     sys.exit(1)
 
