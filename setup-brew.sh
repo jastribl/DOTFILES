7e758c7e524299b8097e3011dc874b7bf06522ed
Justin Stribling <justinastribling@gmail.com>
2019-02-15 15:41:06 -0500

    split brew setup into list files

--- setup-brew.sh
+++ setup-brew.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "---------- installing brew stuff ----------"
 
@@ -8,70 +8,16 @@ else
     brew update
 fi
 
-apps=(
-    gcc
-    git
-    htop
-    pandoc
-    ssh-copy-id
-    the_silver_searcher
-    "vim --with-override-system-vi"
-    wget
-    tmux
-    cask
-    node
-    reattach-to-user-namespace
-    diff-so-fancy
-    watch
-    "go --cross-compile-common"
-    python3
-    composer
-    ctop
-    bash-completion@2
-    cmake
-    mosh
-    boost
-    clang-format
-)
+IFS=$'\n' # split on newline
+apps=($(cat brew/brew-list))
 
 for app in "${apps[@]}"; do
     echo "> brew install $app"
     brew install "$app"
 done
 
-caskapps=(
-    sublime-text
-    ccleaner
-    flux
-    puush
-    iterm2
-    google-chrome
-    google-backup-and-sync
-    skim
-    slack
-    spectacle
-    java8
-    intellij-idea
-    clion
-    xquartz
-    virtualbox
-    isyncr-desktop
-    4k-youtube-to-mp3
-    android-file-transfer
-    messenger
-    postman
-    dash
-    mactex
-    microsoft-office
-    disk-inventory-x
-    phpstorm
-    goland
-    skype
-    vagrant
-    discord
-    osxfuse
-    tunnelblick
-)
+IFS=$'\n' # split on newline
+caskapps=($(cat brew/cask-list))
 
 for app in "${caskapps[@]}"; do
     echo "brew cask install $app"
