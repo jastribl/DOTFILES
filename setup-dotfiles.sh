811c43157735856d0a5452aea5612faaec1df0ec
Justin Stribling <justinastribling@gmail.com>
2019-02-20 15:56:37 -0500

    Removed echos from dotfiles setup script

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -15,18 +15,13 @@ for file in $files; do
     read -p "Would you like to install $file (y/n/q)? " choice
     case "$choice" in
         y|Y )
-            echo "> mv ~/.$file $backupDir/"
             mv ~/.$file $backupDir/
-            echo "> ln -s $dotfileDir/$file ~/.$file"
             ln -s $dotfileDir/$file ~/.$file
             if [ -d "$file.local" ]; then
-                echo "enter the name of the local dotfile you would like to use\n"
+                echo "enter the name of the local dotfile you would like to use"
                 select localFolderName in `ls $file.local/`; do
-                    echo "checking: $file.local/$localFolderName/$file"
                     if [[ -e "$file.local/$localFolderName/$file" ]]; then
-                        echo "> mv ~/.$file.local $backupDir/"
                         mv ~/.$file.local $backupDir/
-                        echo "> ln -s $dotfileDir/$file.local/$localFolderName/$file ~/.$file.local"
                         ln -s $dotfileDir/$file.local/$localFolderName/$file ~/.$file.local
                         break;
                     fi
