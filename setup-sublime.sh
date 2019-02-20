787f3831d437cec634c19a8e18268cd9a2a309f6
Justin Stribling <justinastribling@gmail.com>
2019-02-20 11:44:13 -0500

    Fixed setup-sublime script

--- setup-sublime.sh
+++ setup-sublime.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+rm -rf "/Users/$(whoami)/Library/Application Support/Sublime Text 3/Packages/User"
 ln -f -s $PWD/Sublime/User "/Users/$(whoami)/Library/Application Support/Sublime Text 3/Packages/User"
