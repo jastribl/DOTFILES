42e9cc5893d11a9947a011b21e8e378b815be2ee
Justin Stribling <justinastribling@gmail.com>
2019-02-21 13:30:13 -0500

    Got cleanup script working on ubuntu

--- update-scripts/cleanup-files.sh
+++ update-scripts/cleanup-files.sh
@@ -7,7 +7,7 @@ dirs_to_clean["$HOME/Github"]="-not -type d"
 
 for dir_to_clean in "${!dirs_to_clean[@]}"; do
     rm -f $dir_to_clean/.DS_Store
-    NUM_FILES=`find "$dir_to_clean" ${dirs_to_clean[$dir_to_clean]} -depth 1 | wc -l`
+    NUM_FILES=$(find "$dir_to_clean" -mindepth 1 -maxdepth 1 ${dirs_to_clean[$dir_to_clean]} | wc -l)
     if [ ! $NUM_FILES = 0 ]; then
         echo "Clean up your $dir_to_clean directory!"
     fi
