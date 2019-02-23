b873e1940b02eb97b28ee92d3e6809e9874e533b
Justin Stribling <justinastribling@gmail.com>
2019-02-22 20:26:25 -0500

    Check for broken links for cleanup file

--- update-scripts/cleanup-files.sh
+++ update-scripts/cleanup-files.sh
@@ -7,8 +7,14 @@ dirs_to_clean["$HOME/Github"]="-not -type d"
 
 for dir_to_clean in "${!dirs_to_clean[@]}"; do
     rm -f $dir_to_clean/.DS_Store
-    NUM_FILES=$(find "$dir_to_clean" -mindepth 1 -maxdepth 1 ${dirs_to_clean[$dir_to_clean]} | wc -l)
-    if [ ! $NUM_FILES = 0 ]; then
-        echo "Clean up your $dir_to_clean directory!"
+    extra_files=$(find "$dir_to_clean" -mindepth 1 -maxdepth 1 ${dirs_to_clean[$dir_to_clean]})
+    if [ ! -z "$extra_files" ]; then
+        echo "Clean up your $dir_to_clean directory:"
+        echo "$extra_files"
+    fi
+    broken_links=$(find "$dir_to_clean" -mindepth 1 -maxdepth 1 -type l ! -exec test -e {} \; -print)
+    if [ ! -z "$broken_links" ]; then
+        echo "Broken Links:"
+        echo "$broken_links"
     fi
 done
