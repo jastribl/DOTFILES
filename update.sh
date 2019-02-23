5b227e16f38768620bf7102a0ba5aea58df9f884
Justin Stribling <justinastribling@gmail.com>
2019-02-22 20:17:46 -0500

    Update script rate limiting

--- update.sh
+++ update.sh
@@ -5,7 +5,9 @@ if [ "$PWD" != "$HOME/Github/DOTFILES" ]; then
     exit 1
 fi
 
-usage() { echo "Usage: $0 [-f (force-run-all)]" 1>&2; exit 1; }
+function usage() {
+    echo "Usage: $0 [-f (force)]" 1>&2; exit 1;
+}
 
 force=0
 
@@ -20,34 +22,57 @@ while getopts ":f" o; do
     esac
 done
 
-updateScripts=(
-    update-submodules.sh
-    update-dotfiles.sh
-    update-osx.sh
-    update-vim.sh
-    update-tmux.sh
-    update-powerline-fonts.sh
-    update-diff-so-fancy.sh
-    cleanup-files.sh
-)
+ONE_SECOND=$(( 1 ))
+ONE_MUNUTE=$(( $ONE_SECOND * 60 ))
+ONE_HOUR=$(( $ONE_MUNUTE * 60 ))
+ONE_DAY=$(( $ONE_HOUR * 24 ))
+NOW=$(date +%s)
+UPDATE_SCRIPTS_DIR="update-scripts"
+UPDATE_CACHE_DIR="$UPDATE_SCRIPTS_DIR/cache"
 
-for updateScript in "${updateScripts[@]}"; do
-    run=0
-    if [ $force = 1 ]; then
-        run=1
-    else
-        read -p "Would you like to run $updateScript (y/n/q)? " choice
-        case "$choice" in
-            y|Y ) run=1 ;;
-            n|N ) echo "moving on" ;;
-            q|Q ) exit ;;
-            * ) echo "invalid option" ;;
-        esac
+mkdir -p $UPDATE_CACHE_DIR
+
+declare -a update_scripts
+declare -A update_script_delays
+
+function add_update_script() {
+    update_scripts+=( $1 )
+    update_script_delays[$1]=$2
+}
+
+add_update_script update-submodules.sh $ONE_DAY
+add_update_script update-dotfiles.sh $ONE_DAY
+add_update_script update-osx.sh $ONE_DAY
+add_update_script update-vim.sh $ONE_DAY
+add_update_script update-tmux.sh $ONE_DAY
+add_update_script update-powerline-fonts.sh $(( $ONE_DAY * 30 ))
+add_update_script update-diff-so-fancy.sh $(( $ONE_DAY * 7 ))
+add_update_script cleanup-files.sh $ONE_DAY
+
+
+function run_update_script() {
+    update_script_to_run=$1
+
+    if [[ $force != 1 && -e $UPDATE_CACHE_DIR/$update_script_to_run.last_run ]]; then
+        last_run_time=$(cat $UPDATE_CACHE_DIR/$1.last_run)
+        delay_seconds=${update_script_delays[$update_script_to_run]}
+        must_be_after=$((last_run_time + delay_seconds))
+        if (( $NOW < $must_be_after )); then
+            echo "--- Skipping $update_script_to_run ---"
+            return
+        fi
     fi
 
-    if [ $run = 1 ]; then
-        echo "--- Running $updateScript ---" && ./update-scripts/$updateScript && echo "done"
-    fi
+    echo "--- Running $update_script_to_run ---"
+
+    ./$UPDATE_SCRIPTS_DIR/$update_script_to_run
+    echo $NOW > $UPDATE_CACHE_DIR/$1.last_run
+
+    echo "--- Done running $update_script_to_run ---"
+}
+
+for update_script in "${update_scripts[@]}"; do
+    run_update_script $update_script
 done
 
 touch ~/.hushlogin
