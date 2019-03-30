fa6ecc5f177d8a51d667227fc9960400ca73a407
Justin Stribling <justinastribling@gmail.com>
2019-03-30 17:17:59 -0400

    Remember preferences for local dotfiles

--- update-scripts/update-dotfiles.sh
+++ update-scripts/update-dotfiles.sh
@@ -3,8 +3,14 @@
 DOTFILES_DIR="$PWD/dotfiles"
 BACKUP_DIR="$PWD/backup_dotfiles/backup_$(date +%Y-%m-%d:%H:%M:%S)"
 
+UPDATE_SCRIPTS_DIR="update-scripts"
+UPDATE_CACHE_DIR="$UPDATE_SCRIPTS_DIR/cache"
+SAVED_PREFS_DIR="$UPDATE_CACHE_DIR/prefs"
+
+mkdir -p $SAVED_PREFS_DIR
+
 function backup_dotfile() {
-    existing_dotfile=~/.$1
+    local existing_dotfile=~/.$1
     if [ -L $existing_dotfile ]; then
         rm $existing_dotfile
     elif [ -f $existing_dotfile ]; then
@@ -20,14 +26,20 @@ for file in $all_dotfiles; do
     ln -s $DOTFILES_DIR/$file ~/.$file
 
     if [ -d "$DOTFILES_DIR/$file.local" ]; then
-        echo "enter the name of the local dotfile you would like to use"
-        select localFolderName in $(find $DOTFILES_DIR/$file.local -mindepth 1 -maxdepth 1 -exec basename {} \;); do
-            if [[ -e "$DOTFILES_DIR/$file.local/$localFolderName/$file" ]]; then
-                backup_dotfile $file.local
-                ln -s $DOTFILES_DIR/$file.local/$localFolderName/$file ~/.$file.local
-                break;
-            fi
-        done
+        local_pref_cache_file="$SAVED_PREFS_DIR/$file.local.pref"
+        if [ -f $local_pref_cache_file ]; then
+            localFolderName=$(cat $local_pref_cache_file)
+        else
+            echo "enter the name of the local dotfile you would like to use"
+            select localFolderName in $(find $DOTFILES_DIR/$file.local -mindepth 1 -maxdepth 1 -exec basename {} \;); do
+                if [[ -e "$DOTFILES_DIR/$file.local/$localFolderName/$file" ]]; then
+                    printf "$localFolderName" > $local_pref_cache_file
+                    break;
+                fi
+            done
+        fi
+        backup_dotfile $file.local
+        ln -s $DOTFILES_DIR/$file.local/$localFolderName/$file ~/.$file.local
     elif [ -d "$DOTFILES_DIR/$file.os.local" ]; then
         if [[ $(uname -s) == "Darwin" ]]; then
             os_suffix="osx"
