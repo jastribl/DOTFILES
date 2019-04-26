5c67a6d7bc593f55df0c50bdda8a4faea44b88d1
Justin Stribling <justinastribling@gmail.com>
2019-04-26 02:25:17 -0400

    Moved tmux into dotfiles repo

--- update-scripts/update-tmux.sh
+++ update-scripts/update-tmux.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ ! -e ~/.tmux ]]; then
-    ln -s "$PWD/tmux" ~/.tmux
-fi
 
 # Installs plugins
-./tmux/plugins/tpm/scripts/install_plugins.sh
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 # Updates plugins
-./tmux/plugins/tpm/scripts/update_plugin.sh
+~/.tmux/plugins/tpm/scripts/update_plugin.sh
