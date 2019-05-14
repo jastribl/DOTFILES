fceea9410c1359d6db4a8644316c5f3fa49a28fd
Justin Stribling <justinastribling@gmail.com>
2019-05-14 02:53:19 -0400

    Lots of changes to update-dotfiles scripts including support for per-file custom deploy destinations

--- update-scripts/update-dotfiles.sh
+++ update-scripts/update-dotfiles.sh
@@ -9,46 +9,99 @@ SAVED_PREFS_DIR="$UPDATE_CACHE_DIR/prefs"
 
 mkdir -p $SAVED_PREFS_DIR
 
-function backup_dotfile() {
-    local existing_dotfile=~/.$1
-    if [ -L $existing_dotfile ]; then
-        rm $existing_dotfile
-    elif [ -f $existing_dotfile ]; then
+function backup_and_link() {
+    local source_file=$DOTFILES_DIR/$1
+    local destination_path=$2
+
+    # backup
+    if [ -L $destination_path ]; then
+        rm $destination_path
+    elif [ -f $destination_path ]; then
         mkdir -p $BACKUP_DIR
-        mv $existing_dotfile $BACKUP_DIR/
+        mv $destination_path $BACKUP_DIR/
     fi
+
+    # link
+    mkdir -p $(dirname $destination_path)
+    ln -s $source_file $destination_path
 }
 
-all_dotfiles=$(find $DOTFILES_DIR -mindepth 1 -maxdepth 1 -not -name "*.local" -exec basename {} \;)
-for file in $all_dotfiles; do
-    backup_dotfile $file
+# mappings from file in ./dotfiles/ to path to be placed
+declare -A global_files
+declare -A local_files
+declare -A os_specific_files
 
-    ln -s $DOTFILES_DIR/$file ~/.$file
+function register_global_file() {
+    global_files[$1]=${2:-~/.$1}
+}
+function register_local_file() {
+    local_files[$1]=${2:-~/.$1}
+}
+function register_os_specific_file() {
+    os_specific_files[$1]=${2:-~/.$1}
+}
 
-    if [ -d "$DOTFILES_DIR/$file.local" ]; then
-        local_pref_cache_file="$SAVED_PREFS_DIR/$file.local.pref"
-        if [ -f $local_pref_cache_file ]; then
-            localFolderName=$(cat $local_pref_cache_file)
-        else
-            echo "enter the name of the local dotfile you would like to use"
-            select localFolderName in $(find $DOTFILES_DIR/$file.local -mindepth 1 -maxdepth 1 -exec basename {} \;); do
-                if [[ -e "$DOTFILES_DIR/$file.local/$localFolderName/$file" ]]; then
-                    printf "$localFolderName" > $local_pref_cache_file
-                    break;
-                fi
-            done
-        fi
-        backup_dotfile $file.local
-        ln -s $DOTFILES_DIR/$file.local/$localFolderName/$file ~/.$file.local
-    elif [ -d "$DOTFILES_DIR/$file.os.local" ]; then
-        if [[ $(uname -s) == "Darwin" ]]; then
-            os_suffix="osx"
-        elif [[ $(uname -s) == "Linux" ]]; then
-            os_suffix="linux"
-        else
-            continue;
-        fi
-        backup_dotfile $file.local
-        ln -s $DOTFILES_DIR/$file.os.local/$os_suffix/$file ~/.$file.local
+# todo: try putting this stuff into a config file
+
+# deployed on all machines
+register_global_file bash_profile
+register_global_file bashrc
+register_global_file bashrc.plugables
+register_global_file gitconfig
+register_global_file gitignore_global
+register_global_file inputrc
+register_global_file tmux
+register_global_file tmux.conf
+register_global_file tmux.conf.local
+register_global_file vim
+register_global_file vimrc
+register_global_file ssh/config ~/.ssh/config
+
+# machine-specific deployment
+register_local_file bashrc.local
+
+# os-specific deployment
+register_os_specific_file gitconfig.os
+
+# process all global files
+for file in "${!global_files[@]}"; do
+    backup_and_link $file ${global_files[$file]}
+done
+
+# process all machine-specific files
+for file in "${!local_files[@]}"; do
+    local_pref_cache_file="$SAVED_PREFS_DIR/$file.pref"
+    if [ -f $local_pref_cache_file ]; then
+        local_file_name=$(cat $local_pref_cache_file)
+    else
+        echo "Select the machine-specific dotfile you would like to use"
+        select local_file_name in $(find $DOTFILES_DIR/$file -mindepth 1 -maxdepth 1 -exec basename {} \;); do
+            if [[ -e "$DOTFILES_DIR/$file/$local_file_name" ]]; then
+                printf "$local_file_name" > $local_pref_cache_file
+                break;
+            fi
+        done
+    fi
+
+    backup_and_link $file/$local_file_name ${local_files[$file]}
+done
+
+# process all global files
+OS="$(uname -s)"
+if [[ "$OS" == "Darwin" ]]; then
+    os_suffix="osx"
+elif [[ "$OS" == "Linux" ]]; then
+    os_suffix="linux"
+else
+    echo "**Unable to find matching operating system for '$OS'**"
+    exit
+fi
+
+for file in "${!os_specific_files[@]}"; do
+    local_file_name=$file/$os_suffix
+    if [[ -e "$DOTFILES_DIR/$file/$os_suffix" ]]; then
+        backup_and_link $file/$os_suffix ${os_specific_files[$file]}
+    else
+        echo "**No os-specific version of '$file'**"
     fi
 done
