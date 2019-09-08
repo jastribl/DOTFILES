2971ef8994f130103eb3c1f4c7188d18ce883777
Justin Stribling <justinastribling@gmail.com>
2019-09-08 11:10:15 -0700

    Fix update-diff-so-fancy script to work without proxy

--- update-scripts/update-diff-so-fancy.sh
+++ update-scripts/update-diff-so-fancy.sh
@@ -9,5 +9,5 @@ if [[ ! -e ~/diff-so-fancy ]]; then
     mkdir ~/diff-so-fancy
 fi
 
-wget -O ~/diff-so-fancy/diff-so-fancy https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy > ~/diff-so-fancy/diff-so-fancy
 chmod +x ~/diff-so-fancy/diff-so-fancy
