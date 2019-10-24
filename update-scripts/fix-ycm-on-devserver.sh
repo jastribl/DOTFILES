32a5a41564ec491cf440743f424d6ec42bf86682
Justin Stribling <justinastribling@gmail.com>
2019-10-24 12:12:32 -0700

    Fixed ycm fix script for devserver

--- update-scripts/fix-ycm-on-devserver.sh
+++ update-scripts/fix-ycm-on-devserver.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+
+OUTPUT_PATH="third_party/ycmd/clang_archives/libclang-9.0.0-x86_64-unknown-linux-gnu.tar.bz2"
+DOWNLOAD_LINK="https://dl.bintray.com/ycm-core/libclang/libclang-9.0.0-x86_64-unknown-linux-gnu.tar.bz2"
+
 cd dotfiles/vim/plugged/YouCompleteMe/
-env $(fwdproxy-config --format=sh curl) wget -O third_party/ycmd/clang_archives/libclang-8.0.0-x86_64-unknown-linux-gnu.tar.bz2 https://dl.bintray.com/micbou/libclang/libclang-8.0.0-x86_64-unknown-linux-gnu.tar.bz2
+env $(fwdproxy-config --format=sh curl) wget -O "$OUTPUT_PATH" "$DOWNLOAD_LINK"
 ./install.py --clang-completer
 cd -
