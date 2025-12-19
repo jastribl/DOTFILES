c60e675ee5223353ae9b2bd7513a250702b15d7b
Justin Stribling <justinastribling@gmail.com>
2025-12-19 08:47:08 -0800

    Fix ssh setup for od

--- setup-od.sh
+++ setup-od.sh
@@ -11,6 +11,12 @@ printf 'facebook.devserver' > update-scripts/cache/prefs/ssh/config.local.pref
 printf 'devserver' > update-scripts/cache/prefs/vimrc-plugins.vim.pref
 printf 'facebook' > update-scripts/cache/prefs/vimrc-pre-local.vim.pref
 
+if sed '9!d'  update.sh | grep -q "Update script requires that an ssh key as been generated and added to Github"; then
+    if sed '10!d'  update.sh | grep -q "exit 1"; then
+        sed -i '10d' update.sh
+    fi
+fi
+
 # Run Update
 ./update.sh
 
